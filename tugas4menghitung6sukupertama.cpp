04f7b54e8ea29f9d71fc222e41aad90287925f13
Mohamad Bagoes Ali Yuddin <72675355+MohamadBagoesAliYuddin@users.noreply.github.com>
2020-11-04 14:25:05 +0700

    Add files via upload

---
+++ tugas4menghitung6sukupertama.cpp
@@ -0,0 +1,41 @@
+/*NAMA  : MOHAMAD BAGOES ALI YUDDIN  */
+/*NIM   : 20051397048                */
+/*KELAS : MI_2020B                   */
+
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+   	cout <<"======================================================="<< endl;
+	cout <<"======================================================="<< endl;
+	cout <<"==                                                   =="<< endl;
+	cout <<"==   ---------------MENGHITUNG SUKU---------------   =="<< endl;
+	cout <<"==                                                   =="<< endl;
+	cout <<"======================================================="<< endl;
+	cout <<"======================================================="<< endl << endl;
+	cout <<"Hasil program menghitung 6 suku pertama : "<<endl <<endl;
+    for (a = 1; a < 7; a++)
+    {
+    	b = pow(a,3);
+    	cout << b <<" , ";
+    	
+	}
+	cout <<endl;
+cout <<endl <<"======================================================="<< endl;
+	cout <<   "======================================================="<< endl;
+	cout <<   "==                                                   =="<< endl;
+	cout <<   "==                                                   =="<< endl;
+	cout <<   "==                                                   =="<< endl;
+	cout <<   "==             MOHAMAD BAGOES ALI YUDDIN             =="<< endl;
+	cout <<   "==                    20051397048                    =="<< endl;
+	cout <<   "==                                                   =="<< endl;
+	cout <<   "==                                                   =="<< endl;
+	cout <<   "==                                         Penk_Zpk  =="<< endl;
+	cout <<   "======================================================="<< endl;
+	cout <<   "======================================================="<< endl;	
+}
+
